018024e34560230d2bda2fe1f5301848a803fcee
Joe Hubbert <joe.hubbert@hubnet.cloud>
2022-01-15 13:27:55 +0000

    Created staging table for fact data

---
+++ reporting-on-log-analytics-data/part4/part-4-create-staging-table.sql
@@ -0,0 +1,20 @@
+--Run in the context of reportingDatabase
+
+CREATE TABLE [dbo].[staging_factHubnetCloudWebsiteStats](
+    [timeGenerated] DATETIME2 NULL,
+    [userAction] NVARCHAR(500) NULL,
+    [appUrl] NVARCHAR(500) NULL,
+    [successFlag] BIT NULL,
+    [httpResultCode] INT NULL,
+    [durationOfRequestMs] FLOAT NULL,
+    [clientType] NVARCHAR(50) NULL,
+    [clientOS] NVARCHAR(50) NULL,
+    [clientCity] NVARCHAR(50) NULL,
+    [clientStateOrProvince] NVARCHAR(50) NULL,
+    [clientCountryOrRegion] NVARCHAR(50) NULL,
+    [clientBrowser] NVARCHAR(50) NULL,
+    [appRoleName] NVARCHAR(50) NULL,
+    [requestDate] DATE NULL,
+    [requestHour] TINYINT NULL,
+    [adfPipelineRunId] NVARCHAR(50)
+)

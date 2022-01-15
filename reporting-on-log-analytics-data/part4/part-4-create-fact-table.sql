10069c025443d2602f20bb6c67db742fdc3cb7d0
Joe Hubbert <joe.hubbert@hubnet.cloud>
2022-01-15 13:48:07 +0000

    Added foreign key relationships to fact table

--- reporting-on-log-analytics-data/part4/part-4-create-fact-table.sql
+++ reporting-on-log-analytics-data/part4/part-4-create-fact-table.sql
@@ -7,7 +7,7 @@ CREATE TABLE [dbo].[FactHubnetCloudWebsiteStats](
     [Request_User_Action] NVARCHAR(500) NULL,
     [Request_App_URL] NVARCHAR(500) NULL,
     [Request_Success_Flag] BIT NULL,
-    [Request_Http_Code] INT NULL,
+    [Request_HTTP_Code] INT NULL,
     [Request_Duration_Ms] FLOAT NULL,
     [Request_Client_Type] NVARCHAR(50) NULL,
     [Request_Client_OS] NVARCHAR(50) NULL,
@@ -17,3 +17,12 @@ CREATE TABLE [dbo].[FactHubnetCloudWebsiteStats](
     [Request_Client_Country_Or_Region] NVARCHAR(50) NULL,
     [Request_App_Role_Name] NVARCHAR(50)
 )
+GO
+
+ALTER TABLE [dbo].[FactHubnetCloudWebsiteStats] ADD CONSTRAINT [FK_FactHubnetCloudWebsiteStats_DimDate] FOREIGN KEY([Request_Date_UTC])
+REFERENCES [dbo].[DimDate] ([Date_Value])
+GO
+
+ALTER TABLE [dbo].[FactHubnetCloudWebsiteStats] ADD CONSTRAINT [FK_FactHubnetCloudWebsiteStats_DimHTTPCode] FOREIGN KEY([Request_HTTP_Code])
+REFERENCES [dbo].[DimHTTPCode] ([Http_Code_Key])
+GO

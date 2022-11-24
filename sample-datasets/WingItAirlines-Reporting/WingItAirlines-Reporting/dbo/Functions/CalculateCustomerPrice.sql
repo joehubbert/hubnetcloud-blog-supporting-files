eb05564e88ce7c31ca29f179b7a0c088ce0f9044
Joe Hubbert <joe.hubbert@hubnet.cloud>
2022-11-24 23:09:12 +0000

    Added DACPAC Solution

---
+++ sample-datasets/WingItAirlines-Reporting/WingItAirlines-Reporting/dbo/Functions/CalculateCustomerPrice.sql
@@ -0,0 +1,42 @@
+﻿CREATE FUNCTION [dbo].[CalculateCustomerPrice]
+(
+	@flightScheduleId INT,
+	@ticketTypeId INT
+)
+RETURNS MONEY
+AS
+
+BEGIN
+	DECLARE @flightDate DATE
+	DECLARE @fareRateToApply FLOAT
+	DECLARE @result MONEY
+	DECLARE @ticketTypeDescription NVARCHAR(30)
+
+	--Gets date of flight
+	SET @flightDate = (SELECT CAST([Scheduled_Date_Time_Of_Departure_UTC] AS DATE) FROM [dbo].[FlightSchedule] WHERE [Flight_Schedule_Id] = @flightScheduleId)
+
+	--Looks up the correct ticket type based in integer being passed into function
+	SET @ticketTypeDescription = (SELECT [Ticket_Type] FROM [dbo].[TicketType] WHERE [Ticket_Type_Id] = @ticketTypeId)
+
+	--Gets fare rate based on date of flight
+	SET @fareRateToApply = 
+	(SELECT 
+	CASE
+	WHEN @ticketTypeDescription = 'Economy Class'
+	THEN [Passenger_Fare_Pence_Per_Mile_Economy_Class]
+	WHEN @ticketTypeDescription = 'Premium Economy Class'
+	THEN [Passenger_Fare_Pence_Per_Mile_Premium_Economy_Class]
+	WHEN @ticketTypeDescription = 'Business Class'
+	THEN [Passenger_Fare_Pence_Per_Mile_Business_Class]
+	END
+	FROM [dbo].[PassengerFare] 
+	WHERE @flightDate BETWEEN [Valid_From] AND [Valid_To])
+
+	--Calculates the price that the customer should pay based on miles * the fare rate
+	SET @result = (SELECT SUM(R.[Route_Distance_Nautical_Miles] * @fareRateToApply)
+	FROM [dbo].[FlightSchedule] FS
+	INNER JOIN [dbo].[Route] R ON FS.[Route_Id] = R.[Route_Id]
+	WHERE FS.[Flight_Schedule_Id] = @flightScheduleId)
+
+	RETURN @result
+END
---
+++ sample-datasets/WingItAirlines-Reporting/WingItAirlines-Reporting/dbo/Tables/TicketSale.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE [dbo].[TicketSale]
+(
+	[Ticket_Sale_Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Flight_Schedule_Id] BIGINT NOT NULL,
+	[Ticket_Type_Id] INT NOT NULL,
+	[Ticket_Status_Id] INT NOT NULL,
+	[Ticket_Sale_Timestamp] DATETIME2 NOT NULL,
+	[Customer_Price_Paid] MONEY NOT NULL,
+	[Agency_Id] INT NOT NULL,
+	[Agency_User_Id] INT NOT NULL,
+	[Agency_Commission] MONEY NOT NULL
+)
+GO
+
+ALTER TABLE [dbo].[TicketSale]
+ADD CONSTRAINT [FK_TicketSale_Flight_Schedule]
+FOREIGN KEY([Flight_Schedule_Id])
+REFERENCES [dbo].[FlightSchedule] ([Flight_Schedule_Id])
+GO
+
+ALTER TABLE [dbo].[TicketSale]
+ADD CONSTRAINT [FK_TicketSale_TicketType]
+FOREIGN KEY([Ticket_Type_Id])
+REFERENCES [dbo].[TicketType] ([Ticket_Type_Id])
+GO
+
+ALTER TABLE [dbo].[TicketSale]
+ADD CONSTRAINT [FK_TicketSale_Agency]
+FOREIGN KEY([Agency_Id])
+REFERENCES [dbo].[Agency] ([Agency_Id])
+GO
+
+ALTER TABLE [dbo].[TicketSale]
+ADD CONSTRAINT [FK_TicketSale_AgencyUser]
+FOREIGN KEY([Agency_User_Id])
+REFERENCES [dbo].[AgencyUser] ([Agency_User_Id])
+GO

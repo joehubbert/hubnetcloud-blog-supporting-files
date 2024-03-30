3b390050f624d7c461c8814719ac64bf7e4eda6f
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-03-30 18:33:58 +0000

    Added b2b portal initial code commit

---
+++ power-bi-embedded-reporting/application/b2b-portal/b2b-portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace b2b_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

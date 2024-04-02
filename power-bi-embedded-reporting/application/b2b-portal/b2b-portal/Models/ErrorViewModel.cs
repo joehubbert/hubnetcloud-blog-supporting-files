06ac7f0998a29f3e133ff36841342e51471fae8f
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-04-02 20:27:38 +0100

    Removed b2b portal

--- power-bi-embedded-reporting/application/b2b-portal/b2b-portal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace b2b_portal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

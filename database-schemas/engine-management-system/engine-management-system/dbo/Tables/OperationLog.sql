95e50dcb128fcddbbcc2d7c7406b7ed86f667789
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-12-09 23:23:40 +0100

    Spaces to tabs

--- database-schemas/engine-management-system/engine-management-system/dbo/Tables/OperationLog.sql
+++ database-schemas/engine-management-system/engine-management-system/dbo/Tables/OperationLog.sql
@@ -6,9 +6,9 @@
 	[Job_Cadence] NVARCHAR(50) NOT NULL,
 	[Operation_Timestamp_UTC] DATETIME2 NOT NULL,
 	[Database_Name] NVARCHAR(100) NOT NULL,
-    [Operation_Type] NVARCHAR(50) NOT NULL,
+	[Operation_Type] NVARCHAR(50) NOT NULL,
 	[Operation_Message] NVARCHAR(MAX) NOT NULL, 
-    [Operation_Outcome] NVARCHAR(10) NOT NULL
+	[Operation_Outcome] NVARCHAR(10) NOT NULL
 )
 GO
 

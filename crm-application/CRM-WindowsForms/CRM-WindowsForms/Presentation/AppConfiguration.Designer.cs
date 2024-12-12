1e38f0974f85d37865ca78f8c3de91bfcab62576
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-12-13 00:55:50 +0100

    Added app configuration module

---
+++ crm-application/CRM-WindowsForms/CRM-WindowsForms/Presentation/AppConfiguration.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CRM_WindowsForms.Presentation
+{
+    partial class AppConfiguration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            appConfigurationTitleLabel = new Label();
+            appConfigurationDatabaseEncryptConnectionCheckbox = new CheckBox();
+            appConfigurationDatabaseServernameTextbox = new TextBox();
+            appConfigurationDatabaseNameTextbox = new TextBox();
+            appConfigurationDatabaseServernameLabel = new Label();
+            appConfigurationDatabaseNameLabel = new Label();
+            appConfigurationSaveSettingsButton = new Button();
+            appConfigurationTestConnectionButton = new Button();
+            SuspendLayout();
+            // 
+            // appConfigurationTitleLabel
+            // 
+            appConfigurationTitleLabel.AutoSize = true;
+            appConfigurationTitleLabel.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            appConfigurationTitleLabel.Location = new Point(30, 29);
+            appConfigurationTitleLabel.Name = "appConfigurationTitleLabel";
+            appConfigurationTitleLabel.Size = new Size(605, 48);
+            appConfigurationTitleLabel.TabIndex = 0;
+            appConfigurationTitleLabel.Text = "Application Configuration Settings";
+            // 
+            // appConfigurationDatabaseEncryptConnectionCheckbox
+            // 
+            appConfigurationDatabaseEncryptConnectionCheckbox.AutoSize = true;
+            appConfigurationDatabaseEncryptConnectionCheckbox.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            appConfigurationDatabaseEncryptConnectionCheckbox.Location = new Point(291, 247);
+            appConfigurationDatabaseEncryptConnectionCheckbox.Name = "appConfigurationDatabaseEncryptConnectionCheckbox";
+            appConfigurationDatabaseEncryptConnectionCheckbox.Size = new Size(227, 34);
+            appConfigurationDatabaseEncryptConnectionCheckbox.TabIndex = 1;
+            appConfigurationDatabaseEncryptConnectionCheckbox.Text = "Encrypt Connection";
+            appConfigurationDatabaseEncryptConnectionCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // appConfigurationDatabaseServernameTextbox
+            // 
+            appConfigurationDatabaseServernameTextbox.Location = new Point(291, 123);
+            appConfigurationDatabaseServernameTextbox.Name = "appConfigurationDatabaseServernameTextbox";
+            appConfigurationDatabaseServernameTextbox.Size = new Size(310, 31);
+            appConfigurationDatabaseServernameTextbox.TabIndex = 2;
+            // 
+            // appConfigurationDatabaseNameTextbox
+            // 
+            appConfigurationDatabaseNameTextbox.Location = new Point(291, 184);
+            appConfigurationDatabaseNameTextbox.Name = "appConfigurationDatabaseNameTextbox";
+            appConfigurationDatabaseNameTextbox.Size = new Size(310, 31);
+            appConfigurationDatabaseNameTextbox.TabIndex = 3;
+            // 
+            // appConfigurationDatabaseServernameLabel
+            // 
+            appConfigurationDatabaseServernameLabel.AutoSize = true;
+            appConfigurationDatabaseServernameLabel.Font = new Font("Segoe UI", 11F);
+            appConfigurationDatabaseServernameLabel.Location = new Point(145, 122);
+            appConfigurationDatabaseServernameLabel.Name = "appConfigurationDatabaseServernameLabel";
+            appConfigurationDatabaseServernameLabel.Size = new Size(140, 30);
+            appConfigurationDatabaseServernameLabel.TabIndex = 4;
+            appConfigurationDatabaseServernameLabel.Text = "Server Name";
+            // 
+            // appConfigurationDatabaseNameLabel
+            // 
+            appConfigurationDatabaseNameLabel.AutoSize = true;
+            appConfigurationDatabaseNameLabel.Font = new Font("Segoe UI", 11F);
+            appConfigurationDatabaseNameLabel.Location = new Point(119, 183);
+            appConfigurationDatabaseNameLabel.Name = "appConfigurationDatabaseNameLabel";
+            appConfigurationDatabaseNameLabel.Size = new Size(166, 30);
+            appConfigurationDatabaseNameLabel.TabIndex = 5;
+            appConfigurationDatabaseNameLabel.Text = "Database Name";
+            // 
+            // appConfigurationSaveSettingsButton
+            // 
+            appConfigurationSaveSettingsButton.FlatStyle = FlatStyle.Flat;
+            appConfigurationSaveSettingsButton.Font = new Font("Segoe UI", 11F);
+            appConfigurationSaveSettingsButton.Location = new Point(61, 305);
+            appConfigurationSaveSettingsButton.Name = "appConfigurationSaveSettingsButton";
+            appConfigurationSaveSettingsButton.Size = new Size(310, 84);
+            appConfigurationSaveSettingsButton.TabIndex = 6;
+            appConfigurationSaveSettingsButton.Text = "Save Settings";
+            appConfigurationSaveSettingsButton.UseVisualStyleBackColor = true;
+            appConfigurationSaveSettingsButton.Click += appConfigurationSaveSettingsButton_Click;
+            // 
+            // appConfigurationTestConnectionButton
+            // 
+            appConfigurationTestConnectionButton.FlatStyle = FlatStyle.Flat;
+            appConfigurationTestConnectionButton.Font = new Font("Segoe UI", 11F);
+            appConfigurationTestConnectionButton.Location = new Point(415, 305);
+            appConfigurationTestConnectionButton.Name = "appConfigurationTestConnectionButton";
+            appConfigurationTestConnectionButton.Size = new Size(310, 84);
+            appConfigurationTestConnectionButton.TabIndex = 7;
+            appConfigurationTestConnectionButton.Text = "Test Connection";
+            appConfigurationTestConnectionButton.UseVisualStyleBackColor = true;
+            appConfigurationTestConnectionButton.Click += appConfigurationTestConnectionButton_Click;
+            // 
+            // AppConfiguration
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumPurple;
+            ClientSize = new Size(780, 426);
+            Controls.Add(appConfigurationTestConnectionButton);
+            Controls.Add(appConfigurationSaveSettingsButton);
+            Controls.Add(appConfigurationDatabaseNameLabel);
+            Controls.Add(appConfigurationDatabaseServernameLabel);
+            Controls.Add(appConfigurationDatabaseNameTextbox);
+            Controls.Add(appConfigurationDatabaseServernameTextbox);
+            Controls.Add(appConfigurationDatabaseEncryptConnectionCheckbox);
+            Controls.Add(appConfigurationTitleLabel);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "AppConfiguration";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CRM - App Configuration";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label appConfigurationTitleLabel;
+        private CheckBox appConfigurationDatabaseEncryptConnectionCheckbox;
+        private TextBox appConfigurationDatabaseServernameTextbox;
+        private TextBox appConfigurationDatabaseNameTextbox;
+        private Label appConfigurationDatabaseServernameLabel;
+        private Label appConfigurationDatabaseNameLabel;
+        private Button appConfigurationSaveSettingsButton;
+        private Button appConfigurationTestConnectionButton;
+    }
+}
---
+++ crm-application/CRM-WindowsForms/CRM-WindowsForms/Presentation/AppConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using CRM_WindowsForms.Model;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace CRM_WindowsForms.Presentation
+{
+    public partial class AppConfiguration : Form
+    {
+        private DatabaseConnectionSettings _databaseConnectionSettings;
+
+        public AppConfiguration()
+        {
+            InitializeComponent();
+            LoadConfigurationAsync();
+        }
+
+        private async void LoadConfigurationAsync()
+        {
+            _databaseConnectionSettings = await DatabaseConnectionSettings.LoadAsync();
+            appConfigurationDatabaseServernameTextbox.Text = _databaseConnectionSettings.ServerName;
+            appConfigurationDatabaseNameTextbox.Text = _databaseConnectionSettings.DatabaseName;
+            appConfigurationDatabaseEncryptConnectionCheckbox.Checked = _databaseConnectionSettings.EncryptConnection;
+        }
+
+        private async void appConfigurationSaveSettingsButton_Click(object sender, EventArgs e)
+        {
+            _databaseConnectionSettings.ServerName = appConfigurationDatabaseServernameTextbox.Text;
+            _databaseConnectionSettings.DatabaseName = appConfigurationDatabaseNameTextbox.Text;
+            _databaseConnectionSettings.EncryptConnection = appConfigurationDatabaseEncryptConnectionCheckbox.Checked;
+            await _databaseConnectionSettings.SaveAsync();
+            MessageBox.Show("Database Settings saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void appConfigurationTestConnectionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_databaseConnectionSettings.DatabaseConnectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show("Connection successful.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection failed: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}

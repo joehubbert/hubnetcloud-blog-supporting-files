ba29459faa405e9e59b50c2456d13b60deed0ded
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-12-13 16:43:10 +0100

    Added full functionality to UpdateAccountManager

--- database-schemas/CRM/CRM/dbo/Stored Procedures/UpdateAccountManager.sql
+++ database-schemas/CRM/CRM/dbo/Stored Procedures/UpdateAccountManager.sql
@@ -1,7 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[UpdateAccountManager]
-	@accountManagerId UNIQUEIDENTIFIER
+	@accountManagerId UNIQUEIDENTIFIER,
+	@firstName NVARCHAR(50),
+	@lastName NVARCHAR(50),
+	@emailAddress NVARCHAR(50),
+	@telephoneNumber NVARCHAR(50),
+	@activeStatus BIT
 AS
 
 UPDATE [dbo].[AccountManager]
-
+SET [FirstName] = @firstName,
+	[LastName] = @lastName,
+	[EmailAddress] = @emailAddress,
+	[TelephoneNumber] = @telephoneNumber,
+	[ActiveStatus] = @activeStatus
 WHERE [AccountManagerId] = @accountManagerId

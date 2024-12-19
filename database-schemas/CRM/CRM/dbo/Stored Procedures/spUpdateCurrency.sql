21837fcd6d1e0f12069b69b17119a9fed8fd2cf8
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-12-19 20:15:17 +0000

    Added currency functionality

---
+++ database-schemas/CRM/CRM/dbo/Stored Procedures/spUpdateCurrency.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spUpdateCurrency]
+	@activeStatus BIT,
+	@currencyCode NCHAR(3),
+	@currencyId UNIQUEIDENTIFIER,
+	@currencyName NVARCHAR(50)
+AS
+
+UPDATE [dbo].[Currency]
+SET
+	[ActiveStatus] = @activeStatus,
+	[CurrencyCode] = @currencyCode,
+	[CurrencyName] = @currencyName
+WHERE [CurrencyId] = @currencyId

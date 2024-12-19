1140873c97c54b6d919d1eafc600b131991bded6
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-12-19 19:10:13 +0000

    Added function to calculate shipping date

---
+++ database-schemas/CRM/CRM/dbo/Functions/fnCalculateShippingDate.sql
@@ -0,0 +1,28 @@
+﻿CREATE FUNCTION [dbo].[fnCalculateShippingDate]
+(
+	@deliveryDate DATE,
+	@deliveryMethodId UNIQUEIDENTIFIER
+)
+RETURNS DATE
+AS
+
+BEGIN
+	DECLARE @deliveryTimeDays INT
+	DECLARE @result DATE
+
+	SET @deliveryTimeDays = (SELECT [DeliveryTimeDays] FROM [dbo].[DeliveryMethod] WHERE DeliveryMethodId = @deliveryMethodId)
+
+	WITH 
+	WorkingDaysCTE AS
+	(
+		SELECT @deliveryTimeDays, 
+			DATEADD(DAY, -ROW_NUMBER() OVER (ORDER BY (SELECT NULL)), @deliveryDate) AS WorkingDay
+		FROM [master].sys.all_objects
+		WHERE 
+			TYPE = 'P' 
+			AND DATEPART(WEEKDAY, DATEADD(DAY, -ROW_NUMBER() OVER (ORDER BY (SELECT NULL)), @deliveryDate)) BETWEEN 2 AND 6
+	)
+	SELECT @result = MIN(WorkingDay) FROM WorkingDaysCTE
+
+	RETURN @result
+END

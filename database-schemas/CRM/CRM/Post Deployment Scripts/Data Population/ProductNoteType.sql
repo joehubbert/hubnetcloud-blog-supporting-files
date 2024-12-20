a5a10dc647bafc0d7064d076a110fcb0600fbe81
Joe Hubbert <joe.hubbert@hubnet.cloud>
2024-12-20 01:03:49 +0000

    Added post-deploy scripts

---
+++ database-schemas/CRM/CRM/Post Deployment Scripts/Data Population/ProductNoteType.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE #ProductNoteTypeTemp
+(
+	[ProductNoteType] NVARCHAR(50) NOT NULL
+)
+
+INSERT INTO #ProductNoteTypeTemp ([ProductNoteType]) VALUES ('General')
+INSERT INTO #ProductNoteTypeTemp ([ProductNoteType]) VALUES ('Warranty')
+INSERT INTO #ProductNoteTypeTemp ([ProductNoteType]) VALUES ('Recall')
+INSERT INTO #ProductNoteTypeTemp ([ProductNoteType]) VALUES ('Product Development')
+INSERT INTO #ProductNoteTypeTemp ([ProductNoteType]) VALUES ('Product Improvement')
+INSERT INTO #ProductNoteTypeTemp ([ProductNoteType]) VALUES ('Product Issue')
+
+MERGE INTO [dbo].[ProductNoteType] AS target
+USING #ProductNoteTypeTemp AS source
+ON target.[ProductNoteType] = source.[ProductNoteType]
+WHEN NOT MATCHED THEN
+INSERT
+(
+	[ProductNoteType]
+)
+VALUES
+(
+	source.[ProductNoteType]
+);
+
+DROP TABLE #ProductNoteTypeTemp;

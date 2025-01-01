c9a60871edfe3e00e6ee3779d6c65dc69052836d
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-01-01 14:59:12 +0100

    Updated layout of home

--- crm-application/CRM-WindowsForms/CRM-WindowsForms/Presentation/Home.Designer.cs
+++ crm-application/CRM-WindowsForms/CRM-WindowsForms/Presentation/Home.Designer.cs
@@ -42,10 +42,10 @@
             homeNavOrderManagement.FlatStyle = FlatStyle.Flat;
             homeNavOrderManagement.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             homeNavOrderManagement.ForeColor = Color.Black;
-            homeNavOrderManagement.Location = new Point(320, 167);
-            homeNavOrderManagement.Margin = new Padding(4, 4, 4, 4);
+            homeNavOrderManagement.Location = new Point(249, 141);
+            homeNavOrderManagement.Margin = new Padding(4);
             homeNavOrderManagement.Name = "homeNavOrderManagement";
-            homeNavOrderManagement.Size = new Size(262, 106);
+            homeNavOrderManagement.Size = new Size(208, 73);
             homeNavOrderManagement.TabIndex = 0;
             homeNavOrderManagement.Text = "Order Management";
             homeNavOrderManagement.UseVisualStyleBackColor = false;
@@ -57,10 +57,10 @@
             homeNavAppConfiguration.FlatStyle = FlatStyle.Flat;
             homeNavAppConfiguration.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             homeNavAppConfiguration.ForeColor = Color.Black;
-            homeNavAppConfiguration.Location = new Point(911, 14);
-            homeNavAppConfiguration.Margin = new Padding(4, 4, 4, 4);
+            homeNavAppConfiguration.Location = new Point(685, 13);
+            homeNavAppConfiguration.Margin = new Padding(4);
             homeNavAppConfiguration.Name = "homeNavAppConfiguration";
-            homeNavAppConfiguration.Size = new Size(262, 106);
+            homeNavAppConfiguration.Size = new Size(208, 73);
             homeNavAppConfiguration.TabIndex = 1;
             homeNavAppConfiguration.Text = "App Configuration";
             homeNavAppConfiguration.UseVisualStyleBackColor = false;
@@ -72,10 +72,10 @@
             homeNavProductManagement.FlatStyle = FlatStyle.Flat;
             homeNavProductManagement.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             homeNavProductManagement.ForeColor = Color.Black;
-            homeNavProductManagement.Location = new Point(320, 311);
-            homeNavProductManagement.Margin = new Padding(4, 4, 4, 4);
+            homeNavProductManagement.Location = new Point(249, 240);
+            homeNavProductManagement.Margin = new Padding(4);
             homeNavProductManagement.Name = "homeNavProductManagement";
-            homeNavProductManagement.Size = new Size(262, 106);
+            homeNavProductManagement.Size = new Size(208, 73);
             homeNavProductManagement.TabIndex = 3;
             homeNavProductManagement.Text = "Product Management";
             homeNavProductManagement.UseVisualStyleBackColor = false;
@@ -87,10 +87,10 @@
             homeNavCompanyAdministration.FlatStyle = FlatStyle.Flat;
             homeNavCompanyAdministration.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             homeNavCompanyAdministration.ForeColor = Color.Black;
-            homeNavCompanyAdministration.Location = new Point(605, 311);
-            homeNavCompanyAdministration.Margin = new Padding(4, 4, 4, 4);
+            homeNavCompanyAdministration.Location = new Point(484, 240);
+            homeNavCompanyAdministration.Margin = new Padding(4);
             homeNavCompanyAdministration.Name = "homeNavCompanyAdministration";
-            homeNavCompanyAdministration.Size = new Size(262, 106);
+            homeNavCompanyAdministration.Size = new Size(208, 73);
             homeNavCompanyAdministration.TabIndex = 4;
             homeNavCompanyAdministration.Text = "Company Administration";
             homeNavCompanyAdministration.UseVisualStyleBackColor = false;
@@ -102,10 +102,10 @@
             homeNavCustomerManagement.FlatStyle = FlatStyle.Flat;
             homeNavCustomerManagement.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             homeNavCustomerManagement.ForeColor = Color.Black;
-            homeNavCustomerManagement.Location = new Point(605, 167);
-            homeNavCustomerManagement.Margin = new Padding(4, 4, 4, 4);
+            homeNavCustomerManagement.Location = new Point(484, 141);
+            homeNavCustomerManagement.Margin = new Padding(4);
             homeNavCustomerManagement.Name = "homeNavCustomerManagement";
-            homeNavCustomerManagement.Size = new Size(262, 106);
+            homeNavCustomerManagement.Size = new Size(208, 73);
             homeNavCustomerManagement.TabIndex = 5;
             homeNavCustomerManagement.Text = "Customer Management";
             homeNavCustomerManagement.UseVisualStyleBackColor = false;
@@ -115,19 +115,19 @@
             // 
             homeTitleLabel.AutoSize = true;
             homeTitleLabel.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            homeTitleLabel.Location = new Point(48, 37);
+            homeTitleLabel.Location = new Point(13, 9);
             homeTitleLabel.Margin = new Padding(4, 0, 4, 0);
             homeTitleLabel.Name = "homeTitleLabel";
-            homeTitleLabel.Size = new Size(248, 60);
+            homeTitleLabel.Size = new Size(171, 41);
             homeTitleLabel.TabIndex = 6;
             homeTitleLabel.Text = "CRM Home";
             // 
             // Home
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1187, 499);
+            ClientSize = new Size(925, 343);
             Controls.Add(homeTitleLabel);
             Controls.Add(homeNavCustomerManagement);
             Controls.Add(homeNavCompanyAdministration);
@@ -136,7 +136,7 @@
             Controls.Add(homeNavOrderManagement);
             Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.Fixed3D;
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Home";

b2502b3fb114770d6b3684a35c4b56d8b9772422
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-01-10 22:16:50 +0100

    Added logic to terminate code if there are no changes

--- crm-application/CRM-WindowsForms/CRM-WindowsForms/Presentation/Functions/UpdateConfirmation.cs
+++ crm-application/CRM-WindowsForms/CRM-WindowsForms/Presentation/Functions/UpdateConfirmation.cs
@@ -15,13 +15,21 @@ namespace CRM_WindowsForms.Presentation.Functions
         public static bool ConfirmChanges(List<ChangeDetail> changesList, string dataSubject)
         {
             var changes = new StringBuilder($"Are you sure that you want to update the following values for {dataSubject}?\n\n");
+            bool hasChanges = false;
 
             foreach (var change in changesList)
             {
                 if (!Equals(change.OriginalValue, change.NewValue))
                 {
+                    hasChanges = true;
                     changes.AppendLine($"{change.VariableName}\nOriginal Value: {change.OriginalValue}\nNew Value: {change.NewValue}\n");
                 }
+            }
+
+            if (!hasChanges)
+            {
+                MessageBox.Show("No changes detected. No updates to be made.", "No Updates", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
             }
 
             changes.AppendLine("This action cannot be undone.");

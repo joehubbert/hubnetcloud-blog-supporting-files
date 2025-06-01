2281acab001c4b6a11915fc6bdead14774c1f92f
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-06-01 10:43:10 +0200

    Added about form

---
+++ crm-application/application/windows-forms/CRM-WindowsForms/Presentation/About.cs
@@ -0,0 +1,20 @@
+﻿using CRM_WindowsForms.Presentation.Functions;
+
+namespace CRM_WindowsForms.Presentation
+{
+    partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+            this.aboutProductNameLabel.Text = new AssemblyAccessor().GetAssemblyProduct();
+            this.aboutVersionLabel.Text = $"Version {new AssemblyAccessor().GetAssemblyVersion()}";
+            this.aboutCompanyNameLabel.Text = $"{new AssemblyAccessor().GetAssemblyCompany()} © {DateTime.Now.Year}";
+        }
+
+        private void aboutOKButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

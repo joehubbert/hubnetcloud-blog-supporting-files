6b0fa685ea0c04c9e4da3c04e7bddcafcddc103b
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-08-31 13:29:52 +0200

    Extended numeric check handler

--- crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Presentation/Functions/TextBoxNumericCharacterDataValidationHelper.cs
+++ crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Presentation/Functions/TextBoxNumericCharacterDataValidationHelper.cs
@@ -1,13 +1,51 @@
 ﻿namespace CRM_WindowsForms_EnterpriseEdition.Presentation.Functions
 {
-    internal static class TextBoxNumericCharacterDataValidationHelper
+    internal class TextBoxNumericCharacterDataValidationHelper
     {
-        public static void NumericKeyPressHandler(object sender, KeyPressEventArgs e)
+        private readonly TranslationService _translationService;
+        private string activeRegionLanguageCode;
+        private string errorText;
+
+        private async Task UpdateActiveRegionLanguageCodeAndErrorText()
         {
+            activeRegionLanguageCode = await ApplicationConfigurationService.GetRegionLanguageCodeAsync();
+            if (activeRegionLanguageCode != "en-GB")
+            {
+                errorText = _translationService.Translate("Only numeric characters allowed.", activeRegionLanguageCode);
+            }
+            else
+            {
+                errorText = "Only numeric characters allowed.";
+            }
+        }
+
+        public async Task NumericKeyPressHandlerAsync(object? sender, KeyPressEventArgs e)
+        {
+            activeRegionLanguageCode = await ApplicationConfigurationService.GetRegionLanguageCodeAsync();
+            if (activeRegionLanguageCode != "en-GB")
+            {
+                errorText = _translationService.Translate("Only numeric characters allowed.", activeRegionLanguageCode);
+            }
+            else
+            {
+                errorText = "Only numeric characters allowed.";
+            }
+
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
-                new ErrorMessageService("Warning.DataValidation.Dynamic", "Only numeric characters allowed");
+                new ErrorMessageService("Warning.DataValidation.Dynamic", errorText);
+            }
+        }
+
+        public void NumericKeyPressHandler(object? sender, KeyPressEventArgs e)
+        {
+            UpdateActiveRegionLanguageCodeAndErrorText();
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                new ErrorMessageService("Warning.DataValidation.Dynamic", errorText);
             }
         }
     }

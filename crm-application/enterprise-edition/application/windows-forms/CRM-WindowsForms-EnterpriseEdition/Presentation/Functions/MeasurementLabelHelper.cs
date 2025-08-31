b4a09b9ff24bf4bf66734c6fb43e0c12c0ff7a90
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-08-31 13:51:56 +0200

    Added measurement label helper

---
+++ crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Presentation/Functions/MeasurementLabelHelper.cs
@@ -0,0 +1,59 @@
+﻿namespace CRM_WindowsForms_EnterpriseEdition.Presentation.Functions
+{
+    internal class MeasurementLabelHelper
+    {
+        public static async Task SetUnitLabelAsync(Label label, string measurementType, bool mandatoryFlag, string unitType)
+        {
+            if (label == null)
+                throw new ArgumentNullException(nameof(label));
+
+            switch (measurementType)
+            {
+                case "Area":
+                    if (unitType == "metric")
+                        label.Text += " (m²)";
+                    else
+                        label.Text += " (ft²)";
+                    break;
+                case "Volume":
+                    if (unitType == "metric")
+                        label.Text += " (m³)";
+                    else
+                        label.Text += " (ft³)";
+                    break;
+                case "Distance":
+                    if (unitType == "metric")
+                        label.Text += " (cm)";
+                    else
+                        label.Text += " (in)";
+                    break;
+                case "Temperature":
+                    if (unitType == "metric")
+                        label.Text += " (°C)";
+                    else
+                        label.Text += " (°F)";
+                    break;
+                case "Liquid":
+                    if (unitType == "metric")
+                        label.Text += " (l)";
+                    else
+                        label.Text += " (gal)";
+                    break;
+                case "Weight":
+                    if (unitType == "metric")
+                        label.Text += " (kg)";
+                    else
+                        label.Text += " (lbs)";
+                    break;
+                default:
+                    new ErrorMessageService("Error.Measurement.Type.NotImplemented", measurementType);
+                    break;
+            }
+
+            if (mandatoryFlag)
+            {
+                label.Text += "*";
+            }
+        }
+    }
+}

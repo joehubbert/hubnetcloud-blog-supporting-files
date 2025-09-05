a2ea2c3e115ae793acfc4619114a7fa245555755
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-09-05 17:14:56 +0200

    Removed text from character count helper and added conditional formatting

--- crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Presentation/Functions/TextBoxCharactersRemainingHelper.cs
+++ crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Presentation/Functions/TextBoxCharactersRemainingHelper.cs
@@ -1,46 +1,33 @@
 ﻿namespace CRM_WindowsForms_EnterpriseEdition.Presentation.Functions
 {
-    internal class TextBoxCharactersRemainingHelper
+    internal class TextBoxCharactersRemainingHelper : IDisposable
     {
         private readonly Label _label;
         private readonly int _maximumLength;
         private readonly TextBox _textBox;
-        private readonly TranslationService _translationService;
-        private string activeRegionLanguageCode;
-        private string characterUsage;
+
+        private readonly Color _originalForeColor;
+        private readonly Font _originalFont;
+        private Font? _boldAlertFont;
+        private bool _disposed;
 
         public TextBoxCharactersRemainingHelper(Label label, TextBox textBox)
         {
-            GetActiveRegionLanguageCode();
-
             _label = label ?? throw new ArgumentNullException(nameof(label));
             _textBox = textBox ?? throw new ArgumentNullException(nameof(textBox));
             _maximumLength = _textBox.MaxLength > 0 ? _textBox.MaxLength : int.MaxValue;
 
-            // Hide label initially
+            _originalForeColor = _label.ForeColor;
+            _originalFont = _label.Font;
+
             _label.Visible = false;
 
-            // Subscribe to events
-            _textBox.TextChanged += (sender, e) => UpdateLabel();
-            _textBox.Enter += (sender, e) => ShowLabel();
-            _textBox.Leave += (sender, e) => HideLabel();
+            _textBox.TextChanged += (_, _) => UpdateLabel();
+            _textBox.Enter += (_, _) => ShowLabel();
+            _textBox.Leave += (_, _) => HideLabel();
         }
 
-        private async void GetActiveRegionLanguageCode()
-        {
-            activeRegionLanguageCode = await ApplicationConfigurationService.GetRegionLanguageCodeAsync();
-            string characterUsage = "characters used";
-
-            if (activeRegionLanguageCode != "en-GB")
-            {
-                characterUsage = _translationService.Translate(characterUsage, activeRegionLanguageCode);
-            }
-        }
-
-        private void HideLabel()
-        {
-            _label.Visible = false;
-        }
+        private void HideLabel() => _label.Visible = false;
 
         private void ShowLabel()
         {
@@ -51,7 +38,37 @@
         private void UpdateLabel()
         {
             int currentLength = _textBox.Text.Length;
-            _label.Text = $"{currentLength}/{_maximumLength} {characterUsage}";
+            _label.Text = $"{currentLength}/{_maximumLength}";
+
+            bool atLimit = currentLength == _maximumLength;
+
+            if (atLimit)
+            {
+                // Create the bold font once, re-use it
+                _boldAlertFont ??= new Font(_originalFont, _originalFont.Style | FontStyle.Bold);
+
+                if (!ReferenceEquals(_label.Font, _boldAlertFont))
+                    _label.Font = _boldAlertFont;
+
+                if (_label.ForeColor != Color.Red)
+                    _label.ForeColor = Color.Red;
+            }
+            else
+            {
+                // Restore original styling if it was changed
+                if (!ReferenceEquals(_label.Font, _originalFont))
+                    _label.Font = _originalFont;
+
+                if (_label.ForeColor != _originalForeColor)
+                    _label.ForeColor = _originalForeColor;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _boldAlertFont?.Dispose();
+            _disposed = true;
         }
     }
 }

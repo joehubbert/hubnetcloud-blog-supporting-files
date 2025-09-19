16d9cd5c2e03ed05f5b3b9b0a20624ef6e6e9747
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-09-19 20:49:51 +0200

    Measurement conversion model to support unit conversion

---
+++ crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Model/MeasurementConversionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CRM.Model
+{
+    public class MeasurementConversionModel
+    {
+        public string MeasurementType = string.Empty;
+        public Action<decimal>? SetValue;
+        public decimal Value;
+    }
+}

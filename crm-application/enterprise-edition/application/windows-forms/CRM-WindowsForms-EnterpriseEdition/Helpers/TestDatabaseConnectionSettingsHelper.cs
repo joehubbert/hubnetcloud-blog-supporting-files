0ac2134d66a85af1b55bf07c0c1a42284a6c363c
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-09-19 20:29:03 +0200

    Added helper for testing database connection settings

---
+++ crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Helpers/TestDatabaseConnectionSettingsHelper.cs
@@ -0,0 +1,30 @@
+﻿using CRM.Interface;
+using CRM.Services;
+
+namespace CRM.Helpers
+{
+    public class TestDatabaseConnectionSettingsHelper
+    {
+        public async Task<bool> TestDatabaseConnectionSettings(string dataSubject, DatabaseConnectionSettings databaseConnectionSettings)
+        {
+            bool connectionAvailable = false;
+            try
+            {
+                connectionAvailable = await DBInterface.TestConnectionAsync(databaseConnectionSettings.DatabaseConnectionString);
+            }
+            catch (Exception ex)
+            {
+                new ErrorMessageService("Error.Database.Connection.Failed", dataSubject, ex.Message);
+                return false;
+            }
+
+            if (!connectionAvailable)
+            {
+                new ErrorMessageService("Error.Database.Connection.Failed", dataSubject, "Could not connect to the database.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

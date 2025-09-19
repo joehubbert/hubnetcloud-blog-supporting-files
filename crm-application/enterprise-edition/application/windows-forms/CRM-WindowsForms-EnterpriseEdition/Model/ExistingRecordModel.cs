b5a1c357287a7e84653af57d520908e17379052d
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-09-19 20:50:17 +0200

    Reusable model for existing record handling

---
+++ crm-application/enterprise-edition/application/windows-forms/CRM-WindowsForms-EnterpriseEdition/Model/ExistingRecordModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CRM.Model
+{
+    public class ExistingRecordModel
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedTimestampUTC { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedTimestampUTC { get; set; }
+    }
+}

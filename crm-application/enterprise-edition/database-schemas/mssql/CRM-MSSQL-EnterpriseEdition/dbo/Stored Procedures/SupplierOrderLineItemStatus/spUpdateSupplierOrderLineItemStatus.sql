ad3657e93418cc38b493b9e6ad4d8cf00a9793b7
Joe Hubbert <joe.hubbert@hubnet.cloud>
2025-09-19 19:14:02 +0200

    Organised stored procedures according to functionality

---
+++ crm-application/enterprise-edition/database-schemas/mssql/CRM-MSSQL-EnterpriseEdition/dbo/Stored Procedures/SupplierOrderLineItemStatus/spUpdateSupplierOrderLineItemStatus.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[spUpdateSupplierOrderLineItemStatus]
+	@activeStatus BIT,
+	@supplierOrderLineItemStatus NVARCHAR(50),
+	@supplierOrderLineItemStatusId UNIQUEIDENTIFIER
+AS
+
+BEGIN
+	BEGIN TRY
+		SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+		BEGIN TRANSACTION;
+
+			UPDATE [dbo].[SupplierOrderLineItemStatus]
+			SET 
+				[ActiveStatus] = @activeStatus,
+				[SupplierOrderLineItemStatus] = @supplierOrderLineItemStatus
+			WHERE [SupplierOrderLineItemStatusId] = @supplierOrderLineItemStatusId
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+			ROLLBACK TRANSACTION;
+
+		THROW;
+	END CATCH
+END
